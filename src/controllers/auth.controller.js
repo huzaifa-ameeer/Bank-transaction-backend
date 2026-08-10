faaa15d6aa9feb4bedc3081322ad41d6a1c2c262
Muhammad Huzaifa Ameer <huzaifahnbhai098@gmail.com>
2026-08-10 15:09:52 +0500

    userRegister controller added

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,41 @@
+import userModel from "../models/user.model.js";
+import jwt from "jsonwebtoken";
+
+const userRegister = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const isExists = await userModel.findOne({
+    email: email,
+  });
+
+  if (isExists) {
+    return res.status(409).json({
+      message: "Email already registered",
+      success: false,
+    });
+  }
+
+  const user = await userModel.create({
+    name,
+    email,
+    password,
+  });
+
+  const token = jwt.sign(
+    {
+      userId: user._id,
+    },
+    process.env.JWT_SECRET_KEY,
+    { expiresIn: "3d" },
+  );
+
+  res.cookie("token", token);
+
+  res.status(201).json({
+    message: "User registered successfully",
+    success: true,
+    token,
+  });
+};
+
+export default { userRegister };
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -18,24 +18,24 @@ const userSchema = new mongoose.Schema(
     password: {
       type: String,
       required: [true, "Password is required"],
-      minlength: [6, "Password should not greater than 6 characters"],
+      minlength: [6, "Password should not less than 6 characters"],
       select: false,
     },
   },
   { timestamps: true },
 );
 
-userSchema.pre("save", async function (next){
-    if(!this.isModified("password")) return next()
-    
-    const hash = await bcrypt.hash(this.password, 10)
-    this.password = hash
-    return next()
-})
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return ;
+
+  const hash = await bcrypt.hash(this.password, 10);
+  this.password = hash;
+  return;
+});
 
 userSchema.methods.comparePassword = async function (password) {
-    return await bcrypt.compare(password, this.password)
-}
+  return await bcrypt.compare(password, this.password);
+};
 
 const userModel = mongoose.model("user", userSchema);
 export default userModel;
