607eaa8826dfe3687600bb2c9955be47b4cd6dcc
Muhammad Huzaifa Ameer <huzaifahnbhai098@gmail.com>
2026-08-10 13:29:02 +0500

    connected to DB (mongoDb)

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
+import dotenv from "dotenv";
 import app from "./src/app.js";
+import connectDb from "./src/config/db.js";
 
-const port = process.env.PORT || 3000
+dotenv.config();
+connectDb();
+const port = process.env.PORT || 3000;
 
-app.listen(port, ()=>{
-    console.log(`Server running on: ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Server running on: ${port}`);
+});
