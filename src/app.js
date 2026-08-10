05e949d50aa5eb5c01a39b130186ca07d66f1345
Muhammad Huzaifa Ameer <huzaifahnbhai098@gmail.com>
2026-08-10 22:40:24 +0500

    account router added

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 import express from "express";
-import authRouter from "./routes/auth.route.js";
+import authRouter from "./routes/auth.route.js"
+import accountRouter from "./routes/account.routes.js"
 import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.use(express.json())
 app.use(cookieParser())
+
 app.use("/api/auth", authRouter);
+app.use("/api/account", accountRouter)
 
 export default app;
