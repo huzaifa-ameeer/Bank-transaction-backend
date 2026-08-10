b8ef78b33de1c37d3f881800aae6c4848073703b
Muhammad Huzaifa Ameer <huzaifahnbhai098@gmail.com>
2026-08-10 13:54:01 +0500

    password hashing using bcryptjs

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,27 +1,37 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     email: {
-        type: String,
-        required: [true, "Email is required"],
-        trim: true,
-        lowercase: true,
-        match: [/^[^\s@]+@[^\s@]+\.[^\s@]+$/, "Invalid email address"],
-        unique: [true, "Email already exist"]
+      type: String,
+      required: [true, "Email is required"],
+      trim: true,
+      lowercase: true,
+      match: [/^[^\s@]+@[^\s@]+\.[^\s@]+$/, "Invalid email address"],
+      unique: [true, "Email already exist"],
     },
     name: {
-        type: String,
-        required: [true, "Name is required"]
+      type: String,
+      required: [true, "Name is required"],
     },
     password: {
-        type: String,
-        required: [true, "Password is required"],
-        minlength: [6, "Password should not greater than 6 characters"],
-        select: false
-    }
-},
-{timestamps: true}
-)
+      type: String,
+      required: [true, "Password is required"],
+      minlength: [6, "Password should not greater than 6 characters"],
+      select: false,
+    },
+  },
+  { timestamps: true },
+);
 
-const userModel = mongoose.model("user", userSchema)
-export default userModel
+userSchema.pre("save", async function (next){
+    if(!this.isModified("password")) return next()
+    
+    const hash = await bcrypt.hash(this.password, 10)
+    this.password = hash
+    return next()
+})
+
+const userModel = mongoose.model("user", userSchema);
+export default userModel;
